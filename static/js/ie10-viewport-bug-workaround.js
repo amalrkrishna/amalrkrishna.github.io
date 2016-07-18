355745b5d9bc7d5272da7b7e11f1219b4eb49c99
amalrkrishna <amalrkrishna94@gmail.com>
2016-07-18 18:04:29 +0530

    updated quote

--- static/js/ie10-viewport-bug-workaround.js
+++ static/js/ie10-viewport-bug-workaround.js
@@ -22,19 +22,3 @@
 
 })();
 
-(function() {
-
-    var quotes = $(".quotes");
-    var quoteIndex = -1;
-
-    function showNextQuote() {
-        ++quoteIndex;
-        quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(2000)
-            .delay(2000)
-            .fadeOut(2000, showNextQuote);
-    }
-
-    showNextQuote();
-
-})();​
