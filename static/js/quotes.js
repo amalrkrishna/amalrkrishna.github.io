3405ac49403cac4462f20ed43da1268b230be139
amalrkrishna <amalrkrishna94@gmail.com>
2016-07-18 18:30:33 +0530

    updated quote

--- static/js/quotes.js
+++ static/js/quotes.js
@@ -1,16 +1,15 @@
-jQuery(document).ready(function($) {
-
-    var quotes = $(".quotes");
+(function() {
+    var quotes = $('#quotes h5');
     var quoteIndex = -1;
-
+    
     function showNextQuote() {
         ++quoteIndex;
         quotes.eq(quoteIndex % quotes.length)
+            .delay(1000)
             .fadeIn(2000)
-            .delay(2000)
+            .delay(1000)
             .fadeOut(2000, showNextQuote);
     }
-
-    showNextQuote();
-
-})();​
+    
+    showNextQuote();    
+})();
